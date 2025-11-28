7d8bf8f43e9b9103bd02565239b1050ca9c26070
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-28 22:09:33 +0500

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+import Antd from 'ant-design-vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -6,6 +6,7 @@ import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router);
+app.use(Antd);
 
 app.mount('#app')
