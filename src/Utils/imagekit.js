f4a876e54073b42e74b994d367d903b42f686b15
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-01 18:16:46 +0500

    update

--- src/Utils/imagekit.js
+++
@@ -1,6 +0,0 @@
-import ImageKit from "imagekit-javascript";
-
-export const imagekit = new ImageKit({
-    publicKey: import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
-    urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
-});
